d3a2a97f811bb68f6896baf10dfdc11457e1907e
batterieAAA <arthur@arthurdeschamps.com>
2025-01-04 16:53:51 -0500

    hehe

--- knight.gd
+++ knight.gd
@@ -1,7 +1,13 @@
 extends CharacterBody2D
 
+@onready var sprite = $AnimatedSprite2D
 
 const SPEED = 100
+
+enum {FALL, RUN, DEATH}
+
+var state = FALL
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,13 +17,16 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y = gravity/8
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		sprite.flip_h = velocity.x < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 
 	move_and_slide()
