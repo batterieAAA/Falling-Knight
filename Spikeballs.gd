399120b8807f64d766b532fd6d0bc4a5401b037c
batterieAAA <arthur@arthurdeschamps.com>
2025-01-07 18:13:07 -0500

    spikeball & ajust. sprite

---
+++ Spikeballs.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var speed = 100.0
+
+
+
+func _physics_process(delta):
+	# Move the coin upwards
+	position.y -= speed * delta
+
+
+
+signal enemy_touched 
+
+func _on_coin_body_entered(body): 
+
+	print("ouch")
+
+	if body is CharacterBody2D: 
+		emit_signal("enemy_touched") 
+		queue_free() # Remove the coin from the scene
+
+
+
