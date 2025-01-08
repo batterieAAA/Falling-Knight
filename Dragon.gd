190cc5f7e49546f804552e5b9d7feafe3551c3d1
batterieAAA <arthur@arthurdeschamps.com>
2025-01-08 17:05:04 -0500

    dragon et projectile

---
+++ Dragon.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+# Export variables to adjust in the editor
+@export var projectile_scene: PackedScene
+
+@export var speed: float = 100.0
+
+@onready var marker_2d = $Marker2D
+@onready var player = get_node("/root/Game/Knight")
+@onready var camera = get_node("/root/Game/Camera2D")
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	position.y -= speed * delta
+	#get_player_position()
+	if is_outside_screen():
+		queue_free()
+
+func shoot_projectile():
+	var projectile = projectile_scene.instantiate()
+	projectile.position = marker_2d.position
+	projectile.direction = (player.global_position - marker_2d.global_position).normalized()
+	add_child(projectile)
+
+signal enemy_touched
+
+func _on_player_body_entered(body):
+	print("ouch")
+	if body is CharacterBody2D:
+		emit_signal("enemy_touched")
+		queue_free() # Remove the enemy from the scene
+
+
+func _on_timer_timeout():
+	shoot_projectile()
+
+func is_outside_screen():
+	var camera_position = camera.global_position
+	var viewport_size = get_viewport().get_visible_rect().size
+	var zoom_factor = camera.zoom
+
+
+
+func _on_timer_2_timeout():
+	queue_free()
---
+++ fireball.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+ #Export variables to adjust in the editor
+@export var speed: float = 100.0
+@export var direction: Vector2 = Vector2(0, 1)
+
+
+func _ready():
+	set_process(true)
+	var player = get_node("/root/Game/Knight")
+	connect("enemy_touched", Callable(player, "_on_enemy_touched"))
+
+func _process(delta):
+	position.y += speed * delta
+	position += direction * speed * delta
+
+
+signal enemy_touched
+
+func _on_player_body_entered(body):
+	print("ouch")
+	if body is CharacterBody2D:
+		emit_signal("enemy_touched")
+		queue_free() # Remove the coin from the scene
+
+
+
+func _on_timer_timeout():
+	queue_free() # Remove the coin from the scene
