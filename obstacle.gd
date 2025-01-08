cde080c40bd6d17e94e98237add01dc7f61190d7
Anthony Labelle-Voyez <anthonylabelle-voyez@greenskygamesstudio.com>
2025-01-08 00:30:52 -0500

    fix enemy going out of screen

--- obstacle.gd
+++ obstacle.gd
@@ -2,31 +2,35 @@ extends Area2D
 
 @export var speed = 100.0
 @onready var camera = $"../../Camera2D" 
-var direction = randi_range(-1,1)
-
-
+var direction = randi_range(-1, 1)
 
 func _physics_process(delta):
 	# Move the coin upwards
 	position.y -= speed * delta
 	position.x += speed * direction * delta
+
+	# Boundary checks
+	# Ensure the character stays within the viewport boundaries, considering the zoom factor
+	var camera_position = camera.global_position
+	var viewport_size = get_viewport().get_visible_rect().size
+	var zoom_factor = camera.zoom
+
+	var half_width = (viewport_size.x / 2) / zoom_factor.x
+	var min_x = camera_position.x - half_width
+	var max_x = camera_position.x + half_width
+
+	if global_position.x < min_x or global_position.x > max_x:
+		direction = -direction
+
 	self.scale.x = -1 if direction < 0 else 1
 
+signal enemy_touched
 
-
-signal enemy_touched 
-
-func _on_coin_body_entered(body): 
-
+func _on_coin_body_entered(body):
 	print("ouch")
-
-	if body is CharacterBody2D: 
-		emit_signal("enemy_touched") 
+	if body is CharacterBody2D:
+		emit_signal("enemy_touched")
 		queue_free() # Remove the coin from the scene
 
-
-
 func _on_timer_timeout():
-	direction = randi_range(-1,1)
-
-
+	direction = randi_range(-1, 1)
