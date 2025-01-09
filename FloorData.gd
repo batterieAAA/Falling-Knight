3ae419bbb3b669b18b6e041284377575a938e5c1
Anthony Labelle-Voyez <anthonylabelle-voyez@greenskygamesstudio.com>
2025-01-08 23:26:45 -0500

    Change floors over time

--- FloorData.gd
+++ FloorData.gd
@@ -3,7 +3,7 @@ class_name FloorData
 
 # Properties
 @export var time_interval: float
-@export var background_textures: Array[Texture2D] = []
+@export var background_color: Color
 @export var enemy_scenes: Array[PackedScene] = []
 @export var min_enemy_spawn_interval: float
 @export var max_enemy_spawn_interval: float
--- GameManager.gd
+++ GameManager.gd
@@ -1,18 +1,37 @@
 extends Node
 
-@export var floor_data: FloorData
+@export var all_floor_data: Array[FloorData]
 @onready var spawner = $"../Spawner" 
 @onready var background = $"../CanvasLayer/ParallaxBackground"
 
+var floorIndex = 0
+
 func _ready():
+	init()
+
+func switch_floor():
+	floorIndex += 1
+	init()
+	
+func init():
+	var floor_data : FloorData
+	if floorIndex >= all_floor_data.size(): 
+		floor_data = all_floor_data[all_floor_data.size() - 1] 
+	else: 
+		floor_data = all_floor_data[floorIndex]
+		
 	# Use the properties
 	var bg1 = background.get_node("ParallaxLayer/Sprite2D")
-	bg1.texture = floor_data.background_textures[0]
+	bg1.modulate = floor_data.background_color
 	var bg2 = background.get_node("ParallaxLayer2/Sprite2D")
-	bg2.texture = floor_data.background_textures[1]
+	bg2.modulate = floor_data.background_color
 	var bg3 = background.get_node("ParallaxLayer3/Sprite2D")
-	bg3.texture = floor_data.background_textures[2]
+	bg3.modulate = floor_data.background_color
 	var bg4 = background.get_node("ParallaxLayer4/Sprite2D")
-	bg4.texture = floor_data.background_textures[3]
-	
+	bg4.modulate = floor_data.background_color
+	$Timer.wait_time = floor_data.time_interval
+	$Timer.start()
 	spawner.initialize(floor_data)
+
+func _on_timer_timeout():
+	switch_floor()
