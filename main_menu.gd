afe44d740e8188d2f34833ae023e8a0e5d331a87
Anthony Labelle-Voyez <anthonylabelle-voyez@greenskygamesstudio.com>
2025-01-11 12:54:30 -0500

    fade music

--- main_menu.gd
+++ main_menu.gd
@@ -5,9 +5,12 @@ extends Control
 @onready var camera_2d = $Camera2D
 @onready var button = $CanvasLayer/Button
 @onready var animation_player = $AnimationPlayer
+@onready var music_animation_player = $AnimationPlayer2
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-
+func _ready():
+	print("play fade in")
+	music_animation_player.play("fade_in")
 
 
 func _on_start_game_button_pressed():
@@ -18,7 +21,7 @@ func _on_start_game_button_pressed():
 	camera_2d.shake_screen()
 	animation_player.play("intro")
 	$AudioStreamPlayer.playing = true
-	
+	music_animation_player.play("outro")
 
 
 func _on_timer_timeout():
